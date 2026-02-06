cecbb90e4c01700d935e7eff0075d093efdb49b8
licheff <licheff.o@gmail.com>
2026-02-06 21:55:52 +0200

    dist

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/apartment-expenses/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
